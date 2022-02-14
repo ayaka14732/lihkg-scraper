e098587ce3d81edc1fc68d553c431080385d293b
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-02-15 02:43:33 +0800

    Update v2

--- README.md
+++ README.md
@@ -1,151 +1,58 @@
-# LIHKG Scraper
+# lihkg-scraper
 
-## Sample result
+A Python script for scraping LIHKG and saving the data to a local file.
 
-```
-等自己發到個夢係完整有頭有尾
-因為知道醒咗下次唔會再發返同樣嘅夢
-今朝試過同菲利拗手瓜
-但情況似全職獵人信長對手剛咁
-係撚咁比佢手拗爆
-最後唔知見到斯路
-個夢就完左
-勁鍾意發夢
-長期有夢發
-有時可以控制到
-印象最深刻係去海洋公園玩完旋轉木馬之後趕纜車
-個世界變咗無咩引力
-可以月球漫步咁彈下彈下行路
-個感覺勁真實
-個陣以為係真
-點知原來係夢
-又試過發好得意嘅夢之後發完好清醒
-我唔肯定有冇醒到
-之後我就同自己講要記實個夢之後講俾fd知因為一定好快唔記得
-點知又繼續訓
-就咩都唔記得哂
-```
+The previous version, which is elegant but no longer work, can be found in the [`v1`](https://github.com/ayaka14732/lihkg-scraper/tree/v1) branch.
 
-## Setup
+## Prerequisites
 
-### Specify proxies
+The script is designed to be executed on a typical Ubuntu 20.04 VPS server.
 
-Save a list of proxies to `proxies.txt`.
-
-You should specify one proxy per line. Each proxy is in the format defined by the Request library. For example:
-
-```
-http://ip1:port1/
-http://ip2:port2/
-...
-```
-
-Or:
-
-```
-http://user1:password1@ip1:port1/
-http://user2:password2@ip2:port2/
-...
-```
-
-### Configure the scraper
-
-Copy `config.template.py` to `config.py`, then change the parameters to meet your need.
-
-You can change the following parameters:
-
-- `first_post`: (`int`) The first post to scrape
-- `last_post`: (`int`) The last post to scrape (exclusive)
-- `thread_count`: (`int`) Number of threads used to scrape the pages
-- `server_address`: (`str`) Listen address of the proxy management server
-- `server_port`: (`int`) Listen port of the proxy management server
-
-### Install dependencies
-
-- Arch Linux: `pacman -S python-requests python-lxml python-beautifulsoup4`
-- openSUSE: `zypper install python3-requests python3-lxml python3-beautifulsoup4`
-- Other systems: `pip install -r requirements.txt`
-
-### Run the scraper
+Install Chrome:
 
 ```sh
-python3 main.py
+sudo apt install -y unzip xvfb libxi6 libgconf-2-4
+curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee -a /etc/apt/sources.list.d/google-chrome.list
+sudo apt update -y
+sudo apt install -y google-chrome-stable
 ```
+
+Install ChromeDriver:
+
+```sh
+wget https://chromedriver.storage.googleapis.com/98.0.4758.80/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+rm -f chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin
+```
+
+Install Xvfb:
+
+```sh
+sudo apt install -y xvfb xserver-xephyr tigervnc-standalone-server x11-utils gnumeric
+```
+
+Install Python dependencies:
+
+```sh
+pip install -r requirements.txt
+```
+
+The script is also tested on Arch Linux. In this case, you can install the dependencies from `pacman`.
 
 ## Usage
 
-### Operate the proxy list
-
-The scraper contains a proxy management server that listens on port `18000`. You can operate the proxy list when the scraper is running by sending requests to that port.
-
-Supported operations:
-
-- `GET`: View the proxies that are currently working properly.
-- `POST`: View the proxies that are currently blocked.
-- `PUT`: Add new proxies to the proxy list from `proxies.txt`. If a proxy is currently in the blocklist, it will be removed from the blocklist.
-
-## Design
-
-### Get data from LIHKG API
-
-The method of obtaining data is roughly equivalent to:
+The script is designed to be executed with a HTTP proxy server with authentication.
 
 ```sh
-curl \
--H "Accept: application/json, text/plain, */*" \
--H "Accept-Language: en-US,en;q=0.5" \
--H "Host: lihkg.com" \
--H "Referer: https://lihkg.com/thread/1081676/page/1" \
--H "User-Agent: Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0" \
--H "X-LI-DEVICE: 6c7a86608385347a9cb7f1d1436199c47e250c49" \
--H "X-LI-DEVICE-TYPE: browser" \
--H "X-LI-LOAD-TIME: 6.135287" \
-https://lihkg.com/api_v2/thread/1081676/page/1\?order\=reply_time
+export FROM_THREAD 800000
+export TO_THREAD 900000
+export PROXY_HOST 127.0.0.1
+export PROXY_PORT 20000
+export PROXY_USER testuser
+export PROXY_PASS test123
+python main.py $FROM_THREAD $TO_THREAD $PROXY_HOST $PROXY_PORT $PROXY_USER $PROXY_PASS
 ```
 
-Significant fields in the result:
-
-- `res["success"]` should be `1`.
-- `res["response"]["total_page"]` is the total pages of the thread.
-- `res["response"]["item_data"]` is the list of messages.
-- `res["response"]["item_data"][0]["msg"]` is the text of the first message. Change `0` to other numbers to get other messages.
-
-### Error handling
-
-HTTP status code:
-
-- `403`: The proxy IP is banned by LIHKG. The scraper should retry with another proxy and permanently remove the current proxy from proxy list.
-- `429`: The proxy IP is temporarily blocked by LIHKG. The scraper should retry with another proxy and temporarily remove the current proxy from proxy list.
-- `404`: The requested thread does not exists. The scraper should move on to the next thread.
-
-### Sanitize
-
-Sample raw text from LIHKG API:
-
-```
-我成日擺好啲公仔，做做下嘢唔覺意望過去都見到佢地望住我<img src="/assets/faces/normal/cry.gif" class="hkgmoji" /> <img src="/assets/faces/normal/cry.gif" class="hkgmoji" /> 點解會咁<img src="/assets/faces/normal/frown.gif" class="hkgmoji" />
-```
-
-Things to deal with:
-
-- Line breaks are represented by `<br/>`
-- There are emoticons in the text, which are represented by `<img/>`
-- The content of the text may be 此回覆已被刪除 (This reply has been deleted)
-
-Algorithm:
-
-- Replace `<br/>` by line break
-- Replace `<img/>` and other HTML tags by space
-- Split the string into parts by the characters which are neither Han characters, letters nor numbers
-- Remove the substrings which does not contain Han characters
-- Remove the substrings which are less than five characters
-- Yield the remaining substrings
-
-Sample result:
-
-```
-我成日擺好啲公仔
-做做下嘢唔覺意望過去都見到佢地望住我
-```
-
-You can also modify the algorithm (in `sanitize.py`) to meet your needs.
+You need to modify the values according to your demand and your configuration.
--- main.py
+++ main.py
@@ -1,75 +1,249 @@
-from config import CONFIG
-import crawler
-import logging
-from proxiesmanager import ProxiesManager
-from proxyset import ProxySet
-import requests
-from requests.exceptions import RequestException
-from resultwriter import ResultWriter
-import threading
-from utils import gen_ranges
+import json
+import os.path
+import random
+import signal
+import string
+import sys
+from tqdm import tqdm
+from typing import TextIO, Tuple
+import zipfile
 
-logger = logging.getLogger()
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, WebDriverException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver, WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-rw = ResultWriter()
-ps = ProxySet()
+def random_string() -> str:
+    '''
+    Generate a random string that is suitable to be an identifier.
+    '''
 
-def crawl_range(start, end):
-    session = requests.Session()
-    proxy = ps.get_proxy()
+    length = random.randrange(10, 20)
+    return ''.join(random.choices(string.ascii_lowercase, k=length))
 
-    for thread_id in range(start, end):
-        page = 1
+def make_filename(from_thread: int) -> str:
+    '''
+    Create the file name using the format `lihkg-{from_thread}.csv`.
+    '''
 
-        while True:
-            try:
-                lines, has_next = crawler.crawl_page(session, proxy, thread_id, page)
-            except crawler.Lihkg404RequestException:
-                # If 404, this thread has been removed, so we move on to the next thread
-                logging.info('Page %d/%d proxy %s responses 404', thread_id, page, proxy)
-                break
-            except crawler.Lihkg429RequestException as e:
-                # If 429, the proxy is rate-limited and should sleep for some time
-                logging.info('Page %d/%d proxy %s responses 429, retry after %d', thread_id, page, proxy, e.retry_after)
-                ps.block_proxy_for(proxy, e.retry_after)
-                proxy = ps.get_proxy()
-                session = requests.Session()
-                continue
-            except crawler.Lihkg403RequestException:
-                # If 403, the proxy is blocked and should no longer be used
-                logging.info('Page %d/%d proxy %s responses 403', thread_id, page, proxy)
-                ps.block_proxy(proxy)
-                proxy = ps.get_proxy()
-                session = requests.Session()
-                continue
-            except RequestException as e:
-                logging.info('Page %d/%d proxy %s error %s', thread_id, page, proxy, str(e))
-                ps.block_proxy(proxy)
-                proxy = ps.get_proxy()
-                session = requests.Session()
-                continue
+    filename = f'lihkg-{from_thread}.csv'
+    print('INFO: Writing to file', filename)
+    return filename
 
-            rw.write_lines(lines)  # Write results to file
-            logging.info('Wrote %d lines from page %d/%d', len(lines), thread_id, page)
+def get_next_page_from_json(obj: object, thread_id: int, page: int) -> Tuple[int, int]:
+    '''
+    Determine the next `thread_id` and `page` from the JSON object.
+    If the current value of `page` is equal to `total_page`, we go to the next thread.
+    Otherwise we increment the value of `page`.
+    '''
 
-            if has_next:
-                page += 1  # Same thread, go to next page
-            else:
-                break  # Move on to the next thread
+    if 'response' not in obj:
+        return thread_id + 1, 1
+
+    total_pages = obj['response'].get('total_page', 0)
+    if page >= total_pages:
+        return thread_id + 1, 1
+
+    return thread_id, page + 1
+
+def get_resume_position(filename: str, from_thread: int) -> Tuple[int, int]:
+    if not os.path.exists(filename):
+        return from_thread, 1
+
+    with open(filename) as f:
+        for line in f:
+            pass  # locate the last line
+    thread_id_str, page_str, obj_str = line.rstrip('\n').split('\t')
+    thread_id = int(thread_id_str)
+    page = int(page_str)
+    obj = json.loads(obj_str)  # will throw an exception if the string is not a valid json object
+
+    thread_id_new, page_new = get_next_page_from_json(obj, thread_id, page)
+    print('INFO: Resuming from thread', thread_id_new, 'page', page_new)
+    return thread_id_new, page_new
+
+def read_command_line() -> Tuple[int, int, str, str, str, str]:
+    '''
+    Read the command line arguments in a fixed format.
+    '''
+
+    from_thread = int(sys.argv[1])
+    to_thread = int(sys.argv[2])
+
+    proxy_host = sys.argv[3]
+    proxy_port = sys.argv[4]
+    proxy_user = sys.argv[5]
+    proxy_pass = sys.argv[6]
+
+    return from_thread, to_thread, proxy_host, proxy_port, proxy_user, proxy_pass
+
+def make_proxy_file(proxy_host: str, proxy_port: str, proxy_user: str, proxy_pass: str) -> str:
+    '''
+    Unfortunately We cannot set proxy with authentication by options directly.
+    So we need to write the proxy to a file and ask Chrome to load the file.
+    This function creates this file on the disk and returns the file name.
+    See https://stackoverflow.com/a/55582859.
+    '''
+
+    manifest_json = """
+{
+    "version": "1.0.0",
+    "manifest_version": 2,
+    "name": "Chrome Proxy",
+    "permissions": [
+        "proxy",
+        "tabs",
+        "unlimitedStorage",
+        "storage",
+        "<all_urls>",
+        "webRequest",
+        "webRequestBlocking"
+    ],
+    "background": {
+        "scripts": ["background.js"]
+    },
+    "minimum_chrome_version":"22.0.0"
+}
+"""
+
+    background_js = """
+var config = {
+    mode: "fixed_servers",
+    rules: {
+    singleProxy: {
+        scheme: "http",
+        host: "%s",
+        port: %s
+    },
+    bypassList: ["localhost"]
+    }
+};
+
+chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+function callbackFn(details) {
+    return {
+        authCredentials: {
+            username: "%s",
+            password: "%s"
+        }
+    };
+}
+
+chrome.webRequest.onAuthRequired.addListener(
+    callbackFn,
+    {urls: ["<all_urls>"]},
+    ['blocking']
+);
+""" % (proxy_host, proxy_port, proxy_user, proxy_pass)
+
+    plugin_file = 'proxy_auth_plugin_' + random_string() + '.zip'
+    print('INFO: Using plugin file', plugin_file)
+
+    with zipfile.ZipFile(plugin_file, 'w') as f:
+        f.writestr('manifest.json', manifest_json)
+        f.writestr('background.js', background_js)
+
+    return plugin_file
+
+def init_browser(plugin_file: str) -> WebDriver:
+    options = webdriver.ChromeOptions()
+    options.add_argument('start-maximized')
+    options.add_argument('disable-blink-features=AutomationControlled')
+    options.add_extension(plugin_file)
+    browser = webdriver.Chrome(options=options)
+    return browser
+
+def init_lihkg_context(browser: WebDriver) -> WebElement:
+    '''
+    We need to open a LIHKG page and then jump to the API URL.
+    This process is wrapped as the LIHKG context.
+    '''
+
+    browser.get('https://lihkg.com/thread/2256553/page/1')
+    body = WebDriverWait(browser, timeout=5).until(
+        EC.presence_of_element_located((By.TAG_NAME, 'body'))
+    )
+    element_id = random_string()
+    browser.execute_script(f'a = document.createElement("a"); a.id = arguments[1]; a.target = "_blank"; arguments[0].appendChild(a)', body, element_id)
+    context = browser.find_element(By.ID, element_id)
+    return context
+
+def get_json(browser: WebDriver, context: WebElement, url: str) -> object:
+    browser.execute_script('arguments[0].href = arguments[1]', context, url)
+    browser.execute_script('arguments[0].click()', context)
+    browser.switch_to.window(browser.window_handles[1])
+    pre = WebDriverWait(browser, timeout=5).until(
+        EC.presence_of_element_located((By.TAG_NAME, 'pre'))
+    )
+    text = pre.text
+    obj = json.loads(text)
+    browser.close()
+    browser.switch_to.window(browser.window_handles[0])
+    return obj
+
+def get_json_of_position(browser: WebDriver, context: WebElement, thread_id: int, page: int) -> object:
+    url = f'https://lihkg.com/api_v2/thread/{thread_id}/page/{page}?order=reply_time'
+    obj = get_json(browser, context, url)
+    return obj
+
+def minimize_json(obj: object) -> str:
+    '''
+    Return the most compact representation of a JSON object.
+    '''
+
+    return json.dumps(obj, separators=(',', ':'), ensure_ascii=False)
+
+def write_file(f: TextIO, obj: object, thread_id: int, page: int) -> None:
+    '''
+    Write a result to the output file.
+    '''
+
+    # ignore keyboard interrupt to ensure the integrity of file
+    old_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+    print(thread_id, page, minimize_json(obj), sep='\t', file=f)
+    signal.signal(signal.SIGINT, old_handler)
+
+def start_browser(filename: str, thread_id: int, to_thread: int, page: int, plugin_file: str, pbar: tqdm):
+    display = Display()
+    display.start()
+
+    browser = init_browser(plugin_file)
+    context = init_lihkg_context(browser)
+
+    has_exception = False
+
+    try:
+        with open(filename, 'a') as f:
+            while thread_id < to_thread:
+                obj = get_json_of_position(browser, context, thread_id, page)
+                write_file(f, obj, thread_id, page)
+                thread_id_new, page = get_next_page_from_json(obj, thread_id, page)
+                if thread_id_new > thread_id:
+                    pbar.update()
+                    thread_id = thread_id_new
+    except (TimeoutException, WebDriverException):
+        has_exception = True
+
+    browser.quit()
+    display.stop()
+
+    return has_exception, thread_id, page
+
+def main():
+    from_thread, to_thread, proxy_host, proxy_port, proxy_user, proxy_pass = read_command_line()
+    plugin_file = make_proxy_file(proxy_host, proxy_port, proxy_user, proxy_pass)
+    filename = make_filename(from_thread)
+    thread_id, page = get_resume_position(filename, from_thread)
+    pbar = tqdm(initial=thread_id-from_thread, total=to_thread-from_thread, smoothing=0.)
+    while True:
+        has_exception, thread_id, page = start_browser(filename, thread_id, to_thread, page, plugin_file, pbar)
+        if not has_exception:
+            break
+    pbar.close()
 
 if __name__ == '__main__':
-    pm = ProxiesManager(ps)
-    pm.start()
-
-    ts = []
-
-    for start, end in gen_ranges(CONFIG['first_post'], CONFIG['last_post'], CONFIG['thread_count']):
-        t = threading.Thread(target=crawl_range, args=(start, end))
-        t.start()
-        ts.append(t)
-
-    for t in ts:
-        t.join()  # Wait until all tasks have finished
-
-    logging.info('Done')
+    main()
